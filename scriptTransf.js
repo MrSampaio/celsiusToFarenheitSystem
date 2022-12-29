97b8abe821d3d3132137a8b27d01f58a472b9b7a
Julio Sampaio <jusampa2@gmail.com>
2022-12-28 22:43:32 -0300

    convertSystem

--- scriptTransf.js
+++ scriptTransf.js
@@ -1,7 +1,3 @@
-// como transformar string em número:
-// let testString = "29"
-// console.log(Number(testString))
-
 
 function calculateCelsiusToFarenheit(val){
     val = val * 9/5 + 32
@@ -17,7 +13,7 @@ function calculateDegree(degree){
     let celsius = degree.toUpperCase().includes("C")
     let farenheit = degree.toUpperCase().includes("F")
 
-    if(!celsius || !farenheit){
+    if(!celsius && !farenheit){
         console.log("Tipo de grau não identificado")
     }
 
@@ -34,81 +30,5 @@ function calculateDegree(degree){
     }
 }
 
-console.log(calculateDegree("20g"))
-
-
-
-
-
-
-
-
-
-// function testCelsius(cDegrees){
-//     let celciusDegrees = cDegrees.toUpperCase().includes("C")
-// }
-
-// function testCelsius(cDegrees){
-//  
-//
-//
-//}
-//
-//
-//
-//
-//
-//
-//
-// function testFarenheit(fDegrees){
-//     let farenheitDegrees = fDegrees.toUpperCase().includes("F")
-// }
-// function calculateDegree(){
-
-//     testCelsius(celsius)
-//     testFarenheit(farenheit)
-//     if(!testCelsius && !testFarenheit){
-//         throw new Error ("Tipo de grau não idenficado!")
-//     }
-// }
-
-// console.log(calculateDegree())
-// //marcação aqui
-// // let celsius = "22";
-// // let farenheit = "70";
-
-
-// function transformToAny(){
- 
-//     if(celsius!=="0" && farenheit!=="0"){
-
-//         Number(celsius)
-//         let F = celsius * 9/5 + 32
-
-//         Number(farenheit)
-//         let C = (farenheit - 32) * 5/9  
-
-//         console.log("A temperatura de graus Farenheit em graus Celsius é: " + C + "°C")
-//         console.log("A temperatura em graus Celsius em graus Farenheit é: " + F + "°F")
-//     }
-//     else if(celsius!=="0"){
-
-//         Number(celsius)
-//         let F = celsius * 9/5 + 32;
-
-//         console.log("A temperatura em graus Farenheit é: " + F + "°F")
-//     }
-//     else if(farenheit!=="0"){
-
-//         Number(farenheit)
-//         let C = (farenheit - 32) * 5/9;
-
-//         console.log("A temperatura em graus Celsius é: " + C + "°C")
-//     }
-    
-
-
-// }
-
-
-// console.log(transformToAny())
+console.log(calculateDegree("20D"))
+console.log(calculateDegree("70f"))
